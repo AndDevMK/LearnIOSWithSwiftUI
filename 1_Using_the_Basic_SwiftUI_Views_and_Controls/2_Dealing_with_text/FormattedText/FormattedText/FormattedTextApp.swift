aac829bb9a961385c528d97c13858209d3835a18
pengmj <15820192202@126.com>
2024-10-24 14:16:48 +0800

    Dealing with text

---
+++ 1_Using_the_Basic_SwiftUI_Views_and_Controls/2_Dealing_with_text/FormattedText/FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by 彭明健 on 2024/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
